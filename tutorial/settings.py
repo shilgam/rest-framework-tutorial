2e133a7f4cbd4af2eda726201c62e0ecff60fbad
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-01-08 09:53:22 +0300

    Set HTML and JSON as default set of return formats

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -52,6 +52,13 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    )
+}
+
 ROOT_URLCONF = 'tutorial.urls'
 
 TEMPLATES = [
