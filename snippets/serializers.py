e61c2042fa3fba35831b12274bac9ea0a0021236
Ilgam Shakirov <ishakirov@ninthdecimal.com>
2019-01-08 21:56:09 +0300

    Create serialiser for User model
    Note: make sure to add a few different users, to use for testing the API:
    $ python manage.py createsuperuser

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
-from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
+from snippets.models import Snippet
 
 
 class SnippetSerializer(serializers.ModelSerializer):
     class Meta:
         model = Snippet
         fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+
+
+class UserSerialiser(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
